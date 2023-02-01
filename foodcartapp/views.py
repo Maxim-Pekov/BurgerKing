d8ac60437f9c4e5826ddb675d52694fa892feeee
Maxim-Pekov <max.pekov@gmail.com>
2023-02-01 17:16:39 +0300

    Extend data validation for def register_order.

--- foodcartapp/views.py
+++ foodcartapp/views.py
@@ -66,37 +66,76 @@ def product_list_api(request):
 def register_order(request):
     order_info = request.data
     print(order_info)
-    try:
-        products = order_info['products']
-    except KeyError:
-        content = {
-            'Error. Required parameter missing : '
-            'Required parameter "products" is missing'
-        }
-        return Response(content, status=status.HTTP_422_UNPROCESSABLE_ENTITY)
-    if not isinstance(products, list):
-        content = {
-            'Error. Format is incorrect. : '
-            f'Parameter "products": "{products}", format is incorrect or not '
-            f'presented, expected "list" format'
-            }
-        return Response(content, status=status.HTTP_422_UNPROCESSABLE_ENTITY)
-    elif products == []:
-        content = {
-            'Error. Value is incorrect : '
-            f'Value "products" of parameter {products} is not supported'
-        }
-        return Response(content, status=status.HTTP_422_UNPROCESSABLE_ENTITY)
+    api_order_keys = {'products': list, 'firstname': str, 'lastname': str, \
+        'phonenumber': str, 'address': str}
 
+    for key, value in api_order_keys.items():
+        try:
+            order_info_value = order_info[key]
+            if not isinstance(order_info_value, value):
+                content = {
+                    'Error. Format is incorrect. : '
+                    f'Parameter "{key}": "{order_info_value}", '
+                    f'format is incorrect or not '
+                    f'presented, expected "{value}" format'
+                }
+                return Response(content,
+                                status=status.HTTP_422_UNPROCESSABLE_ENTITY)
+        except KeyError:
+            content = {
+                'Error. Required parameter missing : '
+                f'Required parameter "{key}" is missing'
+            }
+            return Response(
+                content, status=status.HTTP_422_UNPROCESSABLE_ENTITY
+            )
+        if order_info_value in ([], ''):
+            content = {
+                'Error. Value is incorrect : '
+                f'Value "{key}" of parameter "{order_info_value}" is '
+                f'not supported'
+            }
+            return Response(content,
+                            status=status.HTTP_422_UNPROCESSABLE_ENTITY)
+        if key == 'phonenumber' and order_info_value[2:5] == '000':
+            content = {
+                'Error. Phonenumber is incorrect : '
+                f'Value "{key}" of parameter "{order_info_value}" is '
+                f'not supported'
+            }
+            return Response(content,
+                            status=status.HTTP_422_UNPROCESSABLE_ENTITY)
+        # if key == 'products' and order_info_value[]:
+        #
+        #     content = {
+        #         'Error. Phonenumber is incorrect : '
+        #         f'Value "{key}" of parameter "{order_info_value}" is '
+        #         f'not supported'
+        #     }
+        #     return Response(content,
+        #                     status=status.HTTP_422_UNPROCESSABLE_ENTITY)
     order = Order.objects.create(
         name=order_info['firstname'],
         surname=order_info['lastname'],
         phone=order_info['phonenumber'],
         address=order_info['address']
     )
-    for product_by_order in products:
+
+    product_count = Product.objects.all().count()
+
+    for product_by_order in order_info['products']:
         product_id = product_by_order['product']
         product_qlt = product_by_order['quantity']
+
+        if product_id > product_count:
+            content = {
+                'Error. Id of product is incorrect : '
+                f'Value "{product_id}" of parameter "product[id]" is '
+                f'not supported'
+            }
+            return Response(content,
+                            status=status.HTTP_422_UNPROCESSABLE_ENTITY)
+
         product = Product.objects.get(id=product_id)
         product.order = order
         product.quantity = product_qlt
