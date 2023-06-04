3e33573b667a7d79f9e0ab5dd3a32eee1490f774
Maxim-Pekov <max.pekov@gmail.com>
2023-06-04 03:11:50 +0300

    Delete extra directories.

--- geo_coordinates/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-
-class Address(models.Model):
-    address = models.CharField('Адрес', max_length=200, unique=True)
-    lng = models.FloatField('Долгота', null=True)
-    lat = models.FloatField('Широта', null=True)
-    created_at = models.DateTimeField('Дата запроса', auto_now=True)
--- restaurateur/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class RestaurateurConfig(AppConfig):
-    name = 'restaurateur'
